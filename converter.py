cd3cdcebf6a1f02c5377e67085c371a027c129bd
Vgr E.Barry <vgr255@live.ca>
2015-01-02 22:33:15 -0500

    Improved backwards compatibility and improved error handling.

--- converter.py
+++ converter.py
@@ -5,7 +5,7 @@
 # Please give credit to the original author
 # v0.1 - First release
 # v0.2 - Batch mode added
-# v0.3 - Backwards compatibility with Python 2 - not yet tested
+# v0.3 - Backwards compatibility with Python 2 & improved handling
 
 import getpass
 import config
@@ -13,7 +13,7 @@ import os
 
 path = "C:/Users/%s/AppData/Roaming/SpaceEngineers/Blueprints/local/" % getpass.getuser()
 
-retcodes = [
+retcodes = (
 "Prefab convertion completed successfully.",
 "Error: You cannot name your prefab 'bp.sbc'.",
 "Error: NAME is not defined in config.",
@@ -24,7 +24,7 @@ retcodes = [
 "Error: Could not read file.",
 "Error: %s is not a folder." % config.BATCH_PATH,
 "All prefabs converted successfully.",
-]
+)
 
 def main():
     if config.NAME.lower() == "bp.sbc":
@@ -109,37 +109,42 @@ def do(file, oldlines, oldpath, path, prefab, type):
     else:
         print("Converting Prefab '%s' to Blueprint . . ." % prefab)
 
-    with open(file, "w") as new:
-        new.write(oldlines.pop(0) + oldlines.pop(0))
-        new.write("  <ShipBlueprints>\n    <ShipBlueprint>\n")
-    
-        oldlines = oldlines[2:]
-        new.write(oldlines.pop(0))
-    
-        new.write("        <TypeId>MyObjectBuilder_ShipBlueprintDefinition</TypeId>\n")
-        new.write("        <SubtypeId>%s</SubtypeId>\n" % config.NEW_NAME)
-    
-        oldlines = oldlines[2:]
-        new.write(oldlines.pop(0))
-        new.write("      <DisplayName>%s</DisplayName>\n" % config.DISPLAY_NAME)
-    
-        if oldlines[0][:18] == "      <RespawnShip>" and oldlines[0][-15:] == "</RespawnShip>\n":
-            del oldlines[0]
-
-        while oldlines[:-3]:
-            if oldlines[0][:25] == "              <ShareMode>" and oldlines[0][-13:] == "</ShareMode>\n":
-                oldlines[0] = "              <ShareMode>%s</ShareMode>\n" % config.SHARING
-            if oldlines[0] == "              <PilotRelativeWorld>\n":
-                del oldlines[:4]
-                oldlines[0] = "              <PilotRelativeWorld xsi:nil=\"true\" />\n"
-            if oldlines[0] == "          <ConveyorLines>\n":
-                new.write("          <DampenersEnabled>%s</DampenersEnabled>\n" % "true" if config.DAMPENERS else "false")
-            if oldlines[0][:32] == "              <ConveyorLineType>" and oldlines[0][-20:] == "</ConveyorLineType>\n":
-                new.write("              <Sections />\n")
+    try:
+        with open(file, "w") as new:
+            new.write(oldlines.pop(0) + oldlines.pop(0))
+            new.write("  <ShipBlueprints>\n    <ShipBlueprint>\n")
+        
+            oldlines = oldlines[2:]
             new.write(oldlines.pop(0))
-    
-        new.write("      <WorkshopId>0</WorkshopId>\n      <OwnerSteamId>%s</OwnerSteamId>\n" % str(config.OWNER))
-        new.write("    </ShipBlueprint>\n  </ShipBlueprints>\n</Definitions>")
+        
+            new.write("        <TypeId>MyObjectBuilder_ShipBlueprintDefinition</TypeId>\n")
+            new.write("        <SubtypeId>%s</SubtypeId>\n" % config.NEW_NAME)
+        
+            oldlines = oldlines[2:]
+            new.write(oldlines.pop(0))
+            new.write("      <DisplayName>%s</DisplayName>\n" % config.DISPLAY_NAME)
+        
+            if oldlines[0][:18] == "      <RespawnShip>" and oldlines[0][-15:] == "</RespawnShip>\n":
+                del oldlines[0]
+
+            while oldlines[:-3]:
+                if oldlines[0][:25] == "              <ShareMode>" and oldlines[0][-13:] == "</ShareMode>\n":
+                    oldlines[0] = "              <ShareMode>%s</ShareMode>\n" % config.SHARING
+                if oldlines[0] == "              <PilotRelativeWorld>\n":
+                    del oldlines[:4]
+                    oldlines[0] = "              <PilotRelativeWorld xsi:nil=\"true\" />\n"
+                if oldlines[0] == "          <ConveyorLines>\n":
+                    new.write("          <DampenersEnabled>%s</DampenersEnabled>\n" % "true" if config.DAMPENERS else "false")
+                if oldlines[0][:32] == "              <ConveyorLineType>" and oldlines[0][-20:] == "</ConveyorLineType>\n":
+                    new.write("              <Sections />\n")
+                new.write(oldlines.pop(0))
+        
+            new.write("      <WorkshopId>0</WorkshopId>\n      <OwnerSteamId>%s</OwnerSteamId>\n" % str(config.OWNER))
+            new.write("    </ShipBlueprint>\n  </ShipBlueprints>\n</Definitions>")
+    except IndexError:
+        os.remove(file)
+        print("Could not convert '%s'" % prefab)
+        return
 
     if type == "single":
         os.rename(oldpath, path + config.NEW_NAME)
